6dd10f6df9b5a361e316d559bad8797315f57dd4
Iury Gontijo Silva <iurygon.silva@gmail.com>
2023-12-11 23:59:50 -0300

    Adição das operaçõews

---
+++ operations.js
@@ -0,0 +1,16 @@
+const operations = {
+    sum: function(value1, value2){
+
+    },
+    subtraction: function(value1, value2){
+
+    },
+    multiplication: function(value1, value2){
+        
+    },
+    division: function(value1, value2){
+        
+    }
+};
+
+module.exports = operations;

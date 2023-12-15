c84d9a139348924ee292f086508cfaa41f8be3ef
Iury Gontijo Silva <iurygon.silva@gmail.com>
2023-12-14 23:58:55 -0300

    Adição das operações

--- operations.js
+++ operations.js
@@ -1,16 +1,19 @@
 const operations = {
     sum: function(value1, value2){
-
+        return parseInt(value1) + parseInt(value2)
     },
     subtraction: function(value1, value2){
-
+        return parseInt(value1) - parseInt(value2)
     },
     multiplication: function(value1, value2){
-        
+        return parseInt(value1) * parseInt(value2)
     },
     division: function(value1, value2){
-        
+        return parseInt(value1) / parseInt(value2)
     }
 };
 
 module.exports = operations;
+
+
+console.log(operations.division(10, 5))

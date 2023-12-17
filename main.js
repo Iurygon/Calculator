32d89db2e7fc7ace6d24be6c71edf8a1e26f17a8
Iury Gontijo Silva <iurygon.silva@gmail.com>
2023-12-16 22:57:45 -0300

    Adição de scrip

---
+++ main.js
@@ -0,0 +1,7 @@
+const numbers = document.querySelectorAll('.key__numbers');
+const operators = document.querySelectorAll('.key__operator');
+const clear = document.querySelector('.clear__display');
+const erase = document.querySelector('.erase__number');
+const result = document.querySelector('.key__results');
+
+console.log(numbers)

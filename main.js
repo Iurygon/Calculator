c8d6a736cec1293afcfb0125d640f0aee75b6b5d
Iury Gontijo Silva <iurygon.silva@gmail.com>
2024-02-27 23:41:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-/*OPERATIONS OBJECT*/
-import operations from "./operations.js";
+// /*OPERATIONS OBJECT*/
+// import operations from "./operations.js";
 
 /*DOCUMENT ELEMENTS*/
-const numbers = document.querySelectorAll('.key__numbers');
+const numbers = document.querySelectorAll('.key__number');
 const operators = document.querySelectorAll('.key__operator');
 const clear = document.querySelector('.clear__display');
 const erase = document.querySelector('.erase__number');
 const result = document.querySelector('.key__results');
+
+/*OPERATIONAL NUMBERS*/
+let numberOne = [];
+let numberTwo = [];
+
+for(let i = 0; i <= numbers.length - 1; i++){
+    numbers[i].addEventListener('click', () => {
+        numberOne.push(numbers[i].innerHTML);
+        console.log(...numberOne);
+    });
+};
